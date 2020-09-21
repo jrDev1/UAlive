6637d75f127b7b1dbaf770681f84563d6bb8a595
Jason Jones <jasonjones3d@gmail.com>
2020-09-21 14:07:31 -0400

    Added And, Or, and Negate Generators

---
+++ Core/Editor/Units/Bolt/Generators/NegateGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Bolt;
+
+namespace Lasm.UAlive
+{
+    [UnitGenerator(typeof(Negate))]
+    public sealed class NegateGenerator : UnitGenerator<Negate>
+    {
+        public NegateGenerator(Negate unit) : base(unit)
+        {
+        }
+
+        public override string GenerateValue(ValueInput input)
+        {
+            if (input == Unit.input)
+            {
+                if (Unit.input.hasAnyConnection)
+                {
+                    return (Unit.input.connection.source.unit as Unit).GenerateValue(Unit.input.connection.source);
+                }
+            }
+
+            return base.GenerateValue(input);
+        }
+
+        public override string GenerateValue(ValueOutput output)
+        {
+            if (output == Unit.output)
+            {
+                return "!" + GenerateValue(Unit.input);
+            }
+
+            return base.GenerateValue(output);
+        }
+    }
+}
