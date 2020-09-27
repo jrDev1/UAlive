332eee0bbac7b717f60ff9a9daae7f82b7067464
Jason Jones <jasonjones3d@gmail.com>
2020-09-27 11:32:06 -0400

    Entry Units will now delete themselves if copied or not in a Method.

--- Core/Editor/Units/UAlive/Widgets/EntryUnitWidget.cs
+++ Core/Editor/Units/UAlive/Widgets/EntryUnitWidget.cs
@@ -2,20 +2,48 @@
 using System.Collections.Generic;
 using Ludiq;
 using Bolt;
+using System.Linq;
 
 namespace Lasm.UAlive
 {
     [Widget(typeof(EntryUnit))]
     public class EntryUnitWidget : UnitWidget<EntryUnit>
     {
+        private int count;
+
         public EntryUnitWidget(FlowCanvas canvas, EntryUnit unit) : base(canvas, unit)
         {
 
         }
 
-        public override bool canCopy => false;
-        public override bool canDelete => false;
-
+        private bool isDeleting;
+        public override bool canDelete => isDeleting;
+        
         protected override NodeColorMix color => new NodeColorMix(NodeColor.Blue) { red = 0.3f };
+        public override void HandleInput()
+        {
+            var entries = unit.graph.units.OfType<EntryUnit>();
+            var type = reference?.macro?.GetType();
+            var isMethod = type == typeof(Method);
+            if (entries.Count() > 1 || !isMethod)
+            {
+                var list = entries.ToList();
+                if (unit != list[0] || !isMethod)
+                {
+                    isDeleting = true;
+                    selection.Clear();
+                    selection.Add(isMethod ? list[1] : list[0]);
+                    Delete();
+                }
+                else
+                {
+                    isDeleting = false;
+                }
+
+                GraphWindow.active.Repaint();
+            }
+
+            base.HandleInput();
+        }
     }
 }
