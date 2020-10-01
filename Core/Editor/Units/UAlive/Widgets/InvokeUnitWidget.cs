319032c6739813d64eed1c5f3acea189030e277a
Jason Jones <jasonjones3d@gmail.com>
2020-10-01 11:05:36 -0400

    Can now double click unit to enter Invoke Units graph.

--- Core/Editor/Units/UAlive/Widgets/InvokeUnitWidget.cs
+++ Core/Editor/Units/UAlive/Widgets/InvokeUnitWidget.cs
@@ -12,5 +12,18 @@ namespace Lasm.UAlive
         public InvokeUnitWidget(FlowCanvas canvas, InvokeUnit unit) : base(canvas, unit)
         {
         }
+
+        protected override void OnDoubleClick()
+        {
+            base.OnDoubleClick();
+
+            if (unit.method != null)
+            {
+                if (GraphWindow.active != null)
+                {
+                    GraphWindow.OpenActive(GraphReference.New(unit.method, true));
+                }
+            }
+        }
     } 
 }
